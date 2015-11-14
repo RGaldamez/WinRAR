002c197741d080d503119a897cd2d5fa6d52452f
Ricardo Galdamez <ricardo.j.galdamez@gmail.com>
2015-11-14 14:16:00 -0600

    añadiendo un dialogo de lista de comandos

--- WinRaR/src/winrar/Console.java
+++ WinRaR/src/winrar/Console.java
@@ -5,6 +5,8 @@
  */
 package winrar;
 
+import javax.swing.JDialog;
+
 /**
  *
  * @author rick
@@ -15,7 +17,9 @@ public class Console extends javax.swing.JFrame {
      * Creates new form Console
      */
     public Console() {
+        
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -27,21 +31,112 @@ public class Console extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        ListaComandos = new javax.swing.JDialog();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+        jt_direccion = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jl_directorio = new javax.swing.JList();
+        jt_comando = new javax.swing.JTextField();
+        jb_enter = new javax.swing.JButton();
+        jb_listaComandos = new javax.swing.JButton();
+
+        ListaComandos.setTitle("HELP");
+        ListaComandos.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                ListaComandosWindowClosing(evt);
+            }
+        });
+
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "cd <folder name>: cambiara la ruta actual de la consola a la del folder.", "Ejemplo:", "cd Desktop", " ", "ls: listara los archivos y directorios de la carpeta actual.", " ", "compress <filename.txt> <prioridad> | ... | <filename.txt> <priodidad>:", "comprimira los archivos seleccionados en el orden de prioridad especificado;", "como resultado se tendra un archivo de salida con extension hff.", "Ejemplo:", "compress <texto.hff> <1> | <texto2.hff> <2> |<texto3.hff> <3>", " ", "decompress <filename.hff> <prioridad> | ... | <filename.hff> <prioridad>:", "Ejemplo:", "decompress <texto.hff> <1> | <texto2.hff> <2> | <texto3.hff> <3>" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(jList1);
+
+        javax.swing.GroupLayout ListaComandosLayout = new javax.swing.GroupLayout(ListaComandos.getContentPane());
+        ListaComandos.getContentPane().setLayout(ListaComandosLayout);
+        ListaComandosLayout.setHorizontalGroup(
+            ListaComandosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(ListaComandosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 513, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        ListaComandosLayout.setVerticalGroup(
+            ListaComandosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(ListaComandosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 316, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jt_direccion.setText("Aqui va la direccion actual del cisco C:");
+
+        jl_directorio.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Aqui se listan todos los objetos de un directorio" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jl_directorio);
+
+        jt_comando.setText("Aqui va el comando que se ingresa en la consola (ajustar para que se pueda apretar enter para enviar el comando)");
+
+        jb_enter.setText("Enter");
+
+        jb_listaComandos.setText("Lista de Comandos");
+        jb_listaComandos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jb_listaComandosActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jt_direccion, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jt_comando, javax.swing.GroupLayout.DEFAULT_SIZE, 718, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jb_listaComandos)
+                        .addGap(44, 44, 44)
+                        .addComponent(jb_enter, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jt_direccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jt_comando, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jb_enter)
+                    .addComponent(jb_listaComandos))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ListaComandosWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_ListaComandosWindowClosing
+        this.ListaComandos.dispose();
+    }//GEN-LAST:event_ListaComandosWindowClosing
+
+    private void jb_listaComandosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_listaComandosActionPerformed
+        openDialog(ListaComandos);
+    }//GEN-LAST:event_jb_listaComandosActionPerformed
 
     /**
      * @param args the command line arguments
@@ -77,7 +172,24 @@ public class Console extends javax.swing.JFrame {
             }
         });
     }
+    
+    
+    public void openDialog(JDialog dialog){
+        dialog.setLocationRelativeTo(this);
+        dialog.setModal(true);
+        dialog.pack();
+        dialog.setVisible(true);
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDialog ListaComandos;
+    private javax.swing.JList jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JButton jb_enter;
+    private javax.swing.JButton jb_listaComandos;
+    private javax.swing.JList jl_directorio;
+    private javax.swing.JTextField jt_comando;
+    private javax.swing.JTextField jt_direccion;
     // End of variables declaration//GEN-END:variables
 }
